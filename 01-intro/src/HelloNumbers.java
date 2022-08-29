4948d4512459168a3e7a8047b23737333a9a9d26
wushuzh <wushuzh@outlook.com>
2022-08-29 13:21:35 +0800

    Add var type example

---
+++ 01-intro/src/HelloNumbers.java
@@ -0,0 +1,27 @@
+import javax.security.auth.x500.X500Principal;
+
+class HelloNumbers {
+    public static void main(String[] args) {
+        int x;
+        x = 0;
+        while (x < 10) {
+            System.out.println(x);
+            x = x + 1;
+        }
+        // x = "horse";
+        // String x = "horse";
+    }
+}
+
+/**
+ * 1. Before Java variables can be used, they must be declared.
+ * 2. Java variables must have a specific type.
+ * 3. Java variables types can never change.
+ * 4. In Java, types are verified before the code runs ! If there are type
+ * issues, the code WILL NOT COMPILE.
+ * 
+ * python code can crash due to type errors when it's running:
+ * ```print(5 + "horse")```
+ * think about what may happens to you when you're using an application written
+ * by python
+ */
