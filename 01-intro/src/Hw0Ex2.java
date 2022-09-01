098a53c3804a5339e66d697568d2be52082bfc46
wushuzh <wushuzh@outlook.com>
2022-09-01 13:32:01 +0800

    Add forloop solution for max

--- 01-intro/src/Hw0Ex2.java
+++ 01-intro/src/Hw0Ex2.java
@@ -1,18 +1,37 @@
 public class Hw0Ex2 {
     public static void main(String[] args) {
         int[] intNumbers = new int[] { 9, 2, 15, 2, 22, 10, 6 };
-        System.out.println("The max integer is " + max(intNumbers));
+        System.out.println("Comput the max using While is " + maxInWhileForm(intNumbers));
+        System.out.println("Comput the max using ForLoop is " + maxInForLoop(intNumbers));
+        System.out.println("Feed the function with a null ");
+        maxInWhileForm(new int[] {});
         System.out.println("Feed the function with an empty array:");
-        max(new int[] {});
+        maxInForLoop(null);
+
     }
 
-    private static int max(int[] m) {
+    private static int maxInForLoop(int[] m) {
+        assert m != null : "Array is null !";
         assert m.length > 0 : "Array is empty !";
         int largestSoFar = m[0];
         for (int i = 1; i < m.length; i++) {
             if (m[i] > largestSoFar) {
                 largestSoFar = m[i];
             }
+        }
+        return largestSoFar;
+    }
+
+    private static int maxInWhileForm(int[] m) {
+        assert m != null : "Array is null !";
+        assert m.length > 0 : "Array is empty !";
+        int largestSoFar = m[0];
+        int i = 1;
+        while (i < m.length) {
+            if (m[i] > largestSoFar) {
+                largestSoFar = m[i];
+            }
+            i++;
         }
         return largestSoFar;
     }
